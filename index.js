be720f9f4689e50188e19d88636852429612ee03
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2022-08-01 20:28:05 -0300

    my second commit - Finish first excercise

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let olderPersonName = prompt("Digite o nome da pessoa mais velha")
-let onderPersonAge = prompt("Digite a idade da pessoa mais velha")
+let olderPersonAge = prompt("Digite a idade da pessoa mais velha")
 let youngerPersonName = prompt ("Digite o nome da pessoa mais nova")
 let yougerPersonAge = prompt("Digite a idade da pessoa mais nova")
 
 let ageDiference = olderPersonAge - yougerPersonAge
 
 alert(
-    "Pessoa mais velha: "
+    "Pessoa mais velha: " + olderPersonName + "\nIdade: " + olderPersonAge +
+    "\n\nPessoa mais nova: " + yougerPersonAge + "\nIdade: " + yougerPersonAge +
+    "\n\nDiferença de idade: " + ageDiference
 )
