d887f3573c1ecd5e93fe9f1f2fb776fd9068eb65
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2022-08-01 20:17:53 -0300

    My first commit - Begining JS Excercises

---
+++ index.js
@@ -0,0 +1,10 @@
+let olderPersonName = prompt("Digite o nome da pessoa mais velha")
+let onderPersonAge = prompt("Digite a idade da pessoa mais velha")
+let youngerPersonName = prompt ("Digite o nome da pessoa mais nova")
+let yougerPersonAge = prompt("Digite a idade da pessoa mais nova")
+
+let ageDiference = olderPersonAge - yougerPersonAge
+
+alert(
+    "Pessoa mais velha: "
+)
